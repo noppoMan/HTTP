0e015bd315372716701de43578b744b92c28302c
noppoman <yuki@miketokyo.com>
2016-05-23 16:08:07 -0700

    Bump S4 to 0.7

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/noppoMan/MediaType.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/slimane-swift/URI.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 6)
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 7)
     ]
 )
